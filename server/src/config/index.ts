56a98ba3c97e8bb301cca30134587731e07e522b
Cleomir <cleomir.mg@hotmail.com>
2020-05-14 22:06:01 +0300

    Add env variable

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,3 +5,4 @@ env.config();
 export const DB_URL: string = process.env.MONGO_URL!;
 export const ENVIRONMENT: string = process.env.NODE_ENV!;
 export const SERVER_PORT: number = +process.env.SERVER_PORT!;
+export const JWT_SECRET: string = process.env.JWT_SECRET!;
