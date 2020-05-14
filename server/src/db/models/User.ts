6ae2ac6d3b19f374f0feda0ef5a94fdb91e579f8
Cleomir <cleomir.mg@hotmail.com>
2020-05-14 22:05:16 +0300

    Add db model

---
+++ server/src/db/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema: mongoose.Schema<any> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("users", UserSchema);
