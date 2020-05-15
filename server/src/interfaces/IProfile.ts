8facc8a96605bd61b2e24a9a379a778ef5197369
Cleomir <cleomir.mg@hotmail.com>
2020-05-15 23:40:48 +0300

    Add interface

---
+++ server/src/interfaces/IProfile.ts
@@ -0,0 +1,36 @@
+export default interface IProfile {
+  id: string;
+  handle: string;
+  company?: string;
+  website?: string;
+  location?: string;
+  status: string;
+  skills: string[];
+  bio?: string;
+  github_username?: string;
+  experience?: {
+    title: string;
+    company: string;
+    location?: string;
+    from: Date;
+    to?: Date;
+    current: boolean;
+    description?: string;
+  }[];
+  education?: {
+    school: string;
+    degree: string;
+    field_of_study: string;
+    from: Date;
+    to?: Date;
+    current: boolean;
+    description?: string;
+  }[];
+  social?: {
+    youtube: string;
+    twitter: string;
+    facebook: string;
+    linkedin: string;
+    instagram: string;
+  };
+}
