d4e43917652f09a42f097447bb268f31505c3eb8
Cleomir <cleomir.mg@hotmail.com>
2020-05-16 21:29:38 +0300

    Refactor interface

--- server/src/interfaces/IProfile.ts
+++ server/src/interfaces/IProfile.ts
@@ -1,3 +1,6 @@
+import IExperience from "./IExperience";
+import IEducation from "./IEducation";
+
 export default interface IProfile {
   user: string;
   handle: string;
@@ -8,24 +11,8 @@ export default interface IProfile {
   skills: string[];
   bio?: string;
   github_username?: string;
-  experience?: {
-    title: string;
-    company: string;
-    location?: string;
-    from: Date;
-    to?: Date;
-    current: boolean;
-    description?: string;
-  }[];
-  education?: {
-    school: string;
-    degree: string;
-    field_of_study: string;
-    from: Date;
-    to?: Date;
-    current: boolean;
-    description?: string;
-  }[];
+  education?: IEducation[];
+  experience?: IExperience[];
   social?: {
     youtube: string;
     twitter: string;
