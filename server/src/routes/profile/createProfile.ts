b16b3d02762aad8584ad72c456fff03b85b68b88
Cleomir <cleomir.mg@hotmail.com>
2020-05-16 11:18:12 +0300

    Add routes

---
+++ server/src/routes/profile/createProfile.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import Ajv from "ajv";
+
+import logger from "../../helpers/logger";
+import ProfilesSchema from "../../../json-schemas/profile.json";
+import validateRequest from "../../helpers/validadteRequest";
+import { insertProfile } from "../../db/queries";
+import IProfile from "../../interfaces/IProfile";
+import {
+  PROFILE_EXISTS,
+  PROFILE_HANDLE_EXISTS,
+} from "../../config/custom-error-messages";
+import IUser from "../../interfaces/IUser";
+
+/**
+ * Query user profile
+ * @param req - Request object
+ * @param res - Response object
+ *
+ */
+const createProfile = async (req: Request, res: Response) => {
+  try {
+    // extract request params
+    const profile: IProfile = {
+      user: (req.user as IUser).id!,
+      handle: req.body.handle,
+      company: req.body.company,
+      website: req.body.website,
+      location: req.body.location,
+      status: req.body.status,
+      skills: req.body.skills,
+      bio: req.body.bio,
+      github_username: req.body.github_username,
+      experience: req.body.experience,
+      education: req.body.education,
+      social: req.body.social,
+    };
+    const validationResult:
+      | Ajv.ErrorObject[]
+      | null
+      | undefined = validateRequest(ProfilesSchema, { create: { ...profile } });
+
+    // return validation errors
+    if (validationResult && validationResult.length > 0) {
+      return res.status(400).json({ errors: validationResult });
+    }
+
+    await insertProfile(profile);
+    logger.info(`Profile created for user ${profile.user}`);
+
+    return res.status(201).end();
+  } catch (error) {
+    logger.error("Could not create profile \n", error);
+
+    if (error.message === PROFILE_EXISTS) {
+      return res.status(403).json({ message: PROFILE_EXISTS });
+    } else if (error.message === PROFILE_HANDLE_EXISTS) {
+      return res.status(403).json({ message: PROFILE_HANDLE_EXISTS });
+    }
+
+    return res.status(500).json({ message: "Could not create profile" });
+  }
+};
+
+export default createProfile;
--- server/src/routes/profile/getProfile.ts
+++ server/src/routes/profile/getProfile.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import Ajv from "ajv";
-import { Document } from "mongoose";
 
 import logger from "../../helpers/logger";
 import ProfilesSchema from "../../../json-schemas/profile.json";
 import validateRequest from "../../helpers/validadteRequest";
 import IUser from "../../interfaces/IUser";
+import { NO_USER_PROFILE } from "../../config/custom-error-messages";
 import { findProfileById } from "../../db/queries";
+import IProfile from "../../interfaces/IProfile";
 
 /**
  * Query user profile
@@ -21,25 +22,23 @@ const getProfile = async (req: Request, res: Response) => {
     const validationResult:
       | Ajv.ErrorObject[]
       | null
-      | undefined = validateRequest(ProfilesSchema, { id });
+      | undefined = validateRequest(ProfilesSchema, { get: { user: id } });
 
     // return validation errors
     if (validationResult && validationResult.length > 0) {
       return res.status(400).json({ errors: validationResult });
     }
 
-    const profile: Document | null = await findProfileById(id!);
+    const profile: IProfile | null = await findProfileById(id!);
 
     if (!profile) {
-      return res
-        .status(404)
-        .json({ message: "There is no profile for this user" });
+      return res.status(404).json({ message: NO_USER_PROFILE });
     }
 
-    return res.status(200).end();
+    return res.status(200).json(profile);
   } catch (error) {
     logger.error("Could not query profile \n", error);
-    return res.status(500).end();
+    return res.status(500).json({ message: "Could not find profile" });
   }
 };
 
