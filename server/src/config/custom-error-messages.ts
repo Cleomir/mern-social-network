ca414065b55a1618186db06152888b14c44eb190
Cleomir <cleomir.mg@hotmail.com>
2020-05-16 11:17:07 +0300

    Add custom error messages

---
+++ server/src/config/custom-error-messages.ts
@@ -0,0 +1,4 @@
+export const PROFILE_EXISTS = "Profile already exists";
+export const PROFILE_HANDLE_EXISTS = "Profile handle already exists";
+export const USER_EXISTS = "User already exists";
+export const NO_USER_PROFILE = "There is no profile for this user";
