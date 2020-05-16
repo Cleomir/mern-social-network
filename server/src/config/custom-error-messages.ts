87d137578940cb7b8bfea720c47c193c918e9879
Cleomir <cleomir.mg@hotmail.com>
2020-05-16 17:33:42 +0300

    Add error messages

--- server/src/config/custom-error-messages.ts
+++ server/src/config/custom-error-messages.ts
@@ -1,4 +1,9 @@
+export const INVALID_CREDENTIALS = "email or password incorrect";
+export const NO_PROFILE = "Could not find profile";
+export const NO_USER_PROFILE = "There is no profile for this user";
 export const PROFILE_EXISTS = "Profile already exists";
 export const PROFILE_HANDLE_EXISTS = "Profile handle already exists";
 export const USER_EXISTS = "User already exists";
-export const NO_USER_PROFILE = "There is no profile for this user";
+export const USER_NOT_FOUND = "User not found";
+export const USER_NOT_SAVED = "Could not register the user";
+export const UNABLE_TO_LOGIN = "Could not login";
