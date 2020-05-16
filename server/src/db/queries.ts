a5bbcbbf7ec83ec58dd9efb4c5eedfd4a5cd9ce0
Cleomir <cleomir.mg@hotmail.com>
2020-05-16 11:17:22 +0300

    Add queries

--- server/src/db/queries.ts
+++ server/src/db/queries.ts
@@ -1,8 +1,15 @@
-import User from "./models/User";
+import { Document } from "mongoose";
 
+import User from "./models/User";
+import Profile from "./models/Profile";
 import IUser from "../interfaces/IUser";
 import hashPassword from "../helpers/hashPassword";
-import { Document } from "mongoose";
+import IProfile from "../interfaces/IProfile";
+import {
+  USER_EXISTS,
+  PROFILE_HANDLE_EXISTS,
+  PROFILE_EXISTS,
+} from "../config/custom-error-messages";
 
 /**
  * Query user by email
@@ -13,10 +20,31 @@ export const findUserByEmail = async (email: string): Promise<IUser | null> => {
 };
 
 /**
+ * Query profile by ID
+ * @param id User's id
+ */
+export const findProfileById = async (id: string): Promise<IProfile | null> => {
+  return (Profile.findOne({ user: id }).populate("user", [
+    "name",
+    "avatar",
+  ]) as unknown) as IProfile;
+};
+
+/**
+ * Query profile by handle
+ * @param handle Url handle
+ */
+export const findProfileByHandle = async (
+  handle: string
+): Promise<IProfile | null> => {
+  return (Profile.findOne({ handle }) as unknown) as IProfile;
+};
+
+/**
  * Save new user into Database
  * @param user - New user
  */
-export const insertNewUser = async (
+export const insertUser = async (
   user: IUser
 ): Promise<Document | undefined> => {
   try {
@@ -24,11 +52,11 @@ export const insertNewUser = async (
     const existingUser = await findUserByEmail(user.email);
 
     if (existingUser) {
-      throw new Error("User already exists");
+      throw new Error(USER_EXISTS);
     }
 
     const hashedPassword: string = await hashPassword(password);
-    const newUser = new User({
+    const newUser: Document = new User({
       name,
       email,
       avatar,
@@ -36,8 +64,41 @@ export const insertNewUser = async (
       date,
     });
 
-    return await newUser.save();
+    return newUser.save();
   } catch (error) {
     throw error;
   }
+};
+
+/**
+ * Save new Profile
+ * @param userId User's id
+ * @param fields profile fields
+ */
+export const insertProfile = async (profile: IProfile) => {
+  try {
+    const existingProfile: IProfile | null = await findProfileById(
+      profile.user
+    );
+    if (existingProfile) {
+      throw new Error(PROFILE_EXISTS);
+    }
+
+    const existingHandle: IProfile | null = await findProfileByHandle(
+      profile.handle
+    );
+    if (existingHandle) {
+      throw new Error(PROFILE_HANDLE_EXISTS);
+    }
+
+    const newProfile: Document = new Profile(profile);
+
+    return newProfile.save();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProfile = async (userId: string, fields: object) => {
+  return Profile.findOneAndUpdate({ user: userId }, { $set: fields });
 };
