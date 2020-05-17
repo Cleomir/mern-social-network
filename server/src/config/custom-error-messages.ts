05e439551fcab60e887d7dc9be2ce2fc70f6a23f
Cleomir <cleomir.mg@hotmail.com>
2020-05-17 11:05:46 +0300

    Add message and sort properties

--- server/src/config/custom-error-messages.ts
+++ server/src/config/custom-error-messages.ts
@@ -1,11 +1,13 @@
 export const INVALID_CREDENTIALS = "email or password incorrect";
+export const NO_EXPERIENCE = "Experience not found";
 export const NO_PROFILE = "Could not find profile";
 export const NO_USER_PROFILE = "There is no profile for this user";
 export const PROFILE_EXISTS = "Profile already exists";
 export const PROFILE_HANDLE_EXISTS = "Profile handle already exists";
+export const UNABLE_TO_ADD_EDUCATION = "Could not add education";
+export const UNABLE_TO_ADD_EXPERIENCE = "Could not add experience";
+export const UNABLE_TO_LOGIN = "Could not login";
+export const UNABLE_TO_REMOVE_EXPERIENCE = "Could not remove experience";
 export const USER_EXISTS = "User already exists";
 export const USER_NOT_FOUND = "User not found";
 export const USER_NOT_SAVED = "Could not register the user";
-export const UNABLE_TO_LOGIN = "Could not login";
-export const UNABLE_TO_ADD_EXPERIENCE = "Could not add experience";
-export const UNABLE_TO_ADD_EDUCATION = "Could not add education";
