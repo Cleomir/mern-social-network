303c2891c68d3875d001cd4c49417fc9c1e530dd
Cleomir <cleomir.mg@hotmail.com>
2020-05-20 22:17:16 +0300

    Add model

---
+++ server/src/db/models/Post.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const PostSchema: mongoose.Schema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model("post", PostSchema);
