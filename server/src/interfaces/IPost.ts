5a7b417a76d89e77b6ac83ea54075ec6f97cb71d
Cleomir <cleomir.mg@hotmail.com>
2020-05-23 17:06:10 +0300

    Refactor interface

--- server/src/interfaces/IPost.ts
+++ server/src/interfaces/IPost.ts
@@ -1,8 +1,6 @@
-export default interface IPost {
-  avatar?: string;
-  name?: string;
-  text: string;
-  user: string;
+import IComment from "./IComment";
+
+export default interface IPost extends IComment {
   likes?: { _id?: string; user: string }[];
-  comments?: string[];
+  comments?: IComment[];
 }
