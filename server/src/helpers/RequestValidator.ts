a9269d470c357708845c5f71be3f7593fa03f8e1
Cleomir <cleomir.mg@hotmail.com>
2020-06-10 09:41:26 +0300

    Replace implementation

--- server/src/helpers/RequestValidator.ts
+++ server/src/helpers/RequestValidator.ts
@@ -1,46 +1,56 @@
 import Joi, { ValidationResult } from "@hapi/joi";
 import IPost from "../interfaces/IPost";
+import IComment from "../interfaces/IComment";
 
 export default class RequestValidator {
+  // validation rules
+  private static avatarPattern = Joi.string();
+  private static emailPattern = Joi.string().email();
   private static idPattern = Joi.string()
     .pattern(/^[0-9a-f]{24}$/, "id")
     .required();
-  private static userSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email({}).required(),
-    password: Joi.string().min(8).max(20).required(),
-  })
-    .with("name", ["email", "password"])
-    .with("email", "password");
-  private static postSchema = Joi.object({
-    userId: RequestValidator.idPattern,
-    postId: RequestValidator.idPattern,
-    avatar: Joi.string(),
-    name: Joi.string().min(1),
-    text: Joi.string().min(1).required(),
-  }).with("postId", "userId");
+  private static namePattern = Joi.string().min(2);
+  private static passwordPattern = Joi.string().min(8).max(20);
+  private static textPattern = Joi.string().min(1);
 
   public static validateNewUser(
     name: string,
     email: string,
     password: string
   ): ValidationResult {
-    return this.userSchema.validate({ name, email, password });
+    return Joi.object({
+      name: this.namePattern.required(),
+      email: this.emailPattern.required(),
+      password: this.passwordPattern.required(),
+    }).validate({ name, email, password });
   }
 
   public static validateLogin(
     email: string,
     password: string
   ): ValidationResult {
-    return this.userSchema.validate({ email, password });
+    return Joi.object({
+      email: this.emailPattern.required(),
+      password: this.passwordPattern.required(),
+    }).validate({ email, password });
   }
 
-  public static validateNewPost(post: IPost): ValidationResult {
-    return this.postSchema.validate(post);
+  public static validateNewPostOrComment(
+    post: IPost | IComment
+  ): ValidationResult {
+    return Joi.object({
+      avatar: this.avatarPattern,
+      user: this.idPattern.required(),
+      test: this.textPattern.required(),
+      name: this.namePattern,
+    }).validate(post);
   }
 
   public static validateDeleteComment(postId: string, commentId: string) {
-    return this.postSchema.validate({ postId, commentId });
+    return Joi.object({
+      postId: this.idPattern.required(),
+      commentId: this.idPattern.required(),
+    }).validate({ postId, commentId });
   }
 
   public static validateId(id: string): ValidationResult {
