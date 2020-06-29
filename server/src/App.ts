7cbf81689a27221874cb8293a59014ecfaa35223
Cleomir <cleomir.mg@hotmail.com>
2020-06-29 11:30:23 +0300

    Minor refactoring

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
-
 import helmet from "helmet";
 import passport from "passport";
+
 import JwtHandler from "./authentication/jwt";
 import posts from "./routes/posts";
 import profile from "./routes/profile";
@@ -14,26 +14,22 @@ import requestLogger from "./middleware/requestLogger";
  */
 const app: Application = express();
 
-// remove sensible headers
+// global middleware
 app.use(helmet());
-// json body parser
 app.use(express.json());
-// log requests' body
 app.use(requestLogger());
-// passport config
 app.use(passport.initialize());
 JwtHandler.validate(passport);
+
 // mount root routes
 app.use("/users", users);
 app.use("/profiles", profile);
 app.use("/posts", posts);
+
 // 404 handler
 app.use((req: Request, res: Response) => {
-  const error: Error = new Error("not Found");
-
   return res.status(404).json({
-    message: error.message,
-    status: 404,
+    message: "Page not found",
   });
 });
 
