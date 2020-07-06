a4d00b61ef74b9cf17a7ea441bd0ed3ed98aabd3
Cleomir <cleomir.mg@hotmail.com>
2020-07-06 12:04:50 +0300

    Add checkUndefinedEnv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import app from "./App";
-import { env } from "./config/envVariables";
+import { env, checkUndefinedEnv } from "./config/envVariables";
 import connectToDB from "./database/connection";
 import logger, { logObject } from "./logger";
 
 // bootstrap
 (async () => {
   try {
+    checkUndefinedEnv();
     await connectToDB(env.DB_URL);
   } catch (error) {
     logObject("error", `[MONGO] Connection error`, error);
