fb229f6288e5dfd77d9db6279f76da27a441b1a7
Cleomir <cleomir.mg@hotmail.com>
2020-07-08 17:31:38 +0300

    Add interfaces

--- server/src/interfaces/function-types/IDatabase.ts
+++ server/src/interfaces/function-types/IDatabase.ts
@@ -1,12 +1,23 @@
 import IUser from "../IUser";
 import { Document } from "mongoose";
 import IProfile from "../IProfile";
+import IPost from "../IPost";
 
 export interface IFindOneUser {
-  (whereClause: Record<string, unknown>): Promise<IUser | null>;
+  (whereClause: Record<string, unknown>, requestId: string): Promise<
+    IUser | undefined
+  >;
+}
+
+export interface IFindOnePost {
+  (id: string, requestId: string): Promise<IPost | undefined>;
 }
 
 export interface ISaveOneDocument {
+  (document: Document): Promise<void>;
+}
+
+export interface IDeleteOneDocument {
   (document: Document): Promise<void>;
 }
 
