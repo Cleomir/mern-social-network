f21cdd9b1e1b71f8d7b56f4aedaf3ac2cb5c71bc
Cleomir <cleomir.mg@hotmail.com>
2020-07-10 09:22:41 +0300

    Convert class into functions

--- server/src/authentication/jwt/index.ts
+++ server/src/authentication/jwt/index.ts
@@ -6,30 +6,28 @@ import IJwtPayload from "../../interfaces/IJwtPayload";
 import { env } from "../../config/envVariables";
 import logger from "../../logger";
 
-export default class JwtHandler {
-  public static validate(passport: PassportStatic): void {
-    const options = {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: env.JWT_SECRET,
-    };
+export const validateJWT = (passport: PassportStatic): void => {
+  const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: env.JWT_SECRET,
+  };
 
-    passport.use(
-      new JwtStrategy(options, (jwtPayload, done) => {
-        if (Date.now().valueOf() / 1000 > jwtPayload.exp) {
-          // token expired
-          done("unauthorized");
-        }
-        done(null, jwtPayload);
-      })
-    );
-  }
+  passport.use(
+    new JwtStrategy(options, (jwtPayload, done) => {
+      if (Date.now().valueOf() / 1000 > jwtPayload.exp) {
+        // token expired
+        done("unauthorized");
+      }
+      done(null, jwtPayload);
+    })
+  );
+};
 
-  public static sign(payload: IJwtPayload, requestId: string): string {
-    const options: SignOptions = { expiresIn: "1h" };
-    logger.info(`[JWT][${requestId}] Generating JWT`);
-    const token: string = jwt.sign(payload, env.JWT_SECRET, options);
-    logger.info(`[JWT][${requestId}] JWT generated`);
+export const signJWT = (payload: IJwtPayload, requestId: string): string => {
+  const options: SignOptions = { expiresIn: "1h" };
+  logger.info(`[JWT][${requestId}] Generating JWT`);
+  const token: string = jwt.sign(payload, env.JWT_SECRET, options);
+  logger.info(`[JWT][${requestId}] JWT generated`);
 
-    return token;
-  }
-}
+  return token;
+};
--- server/src/authentication/password/index.ts
+++ server/src/authentication/password/index.ts
@@ -1,30 +1,28 @@
 import bcrypt from "bcryptjs";
 import logger from "../../logger";
 
-export default class PasswordHandler {
-  /**
-   * Compare user password with hashed password
-   * @param password - User's password
-   */
-  public static async compare(
-    plainPassword: string,
-    hashedPassword: string,
-    requestId: string
-  ): Promise<boolean> {
-    const match: boolean = await bcrypt.compare(plainPassword, hashedPassword);
-    if (!match) {
-      logger.info(`[BCRYPT][${requestId}] Password doesn't match`);
-    }
-    logger.info(`[BCRYPT][${requestId}] Password matches`);
-    return match;
+/**
+ * Compare user password with hashed password
+ * @param password - User's password
+ */
+export const comparePassword = async (
+  plainPassword: string,
+  hashedPassword: string,
+  requestId: string
+): Promise<boolean> => {
+  const match: boolean = await bcrypt.compare(plainPassword, hashedPassword);
+  if (!match) {
+    logger.info(`[BCRYPT][${requestId}] Password doesn't match`);
   }
+  logger.info(`[BCRYPT][${requestId}] Password matches`);
+  return match;
+};
 
-  /**
-   * Hash plain text password
-   * @param password - User's password
-   */
-  public static async hash(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt(10);
-    return bcrypt.hash(password, salt);
-  }
-}
+/**
+ * Hash plain text password
+ * @param password - User's password
+ */
+export const hashPassword = async (password: string): Promise<string> => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
