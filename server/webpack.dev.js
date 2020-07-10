47f9eea0d0ad5173637ba1b9d9f30c3f1bd2d481
Cleomir <cleomir.mg@hotmail.com>
2020-07-10 09:21:57 +0300

    Fix merge import

--- server/webpack.dev.js
+++ server/webpack.dev.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
