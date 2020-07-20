89e8f8308861e3869a0922d596fd22c1f6a6ad4f
Cleomir <cleomir.mg@hotmail.com>
2020-07-20 09:01:08 +0300

    Fix and add tests

--- server/tests/integration/profile/create.test.ts
+++ server/tests/integration/profile/create.test.ts
@@ -1,100 +1,65 @@
 import request, { Response } from "supertest";
 import Chance from "chance";
+import { v4 as uuid } from "uuid";
 
 import app from "../../../src/App";
+import { signJWT } from "../../../src/authentication/jwt";
+import createProfileMock from "../../helpers/createProfileMock";
+import * as queries from "../../../src/database/queries";
+import IProfile from "../../../src/interfaces/IProfile";
 
 describe("Test /profiles path", () => {
   const chance = new Chance();
 
-  test.skip("It should create a profile and return status 200", async () => {
-    const name = chance.name();
-    const email = chance.email();
-    const password = chance.string({ length: 8 });
-    const handle = chance.string({ length: 20 });
-    const company = chance.string();
-    const website = chance.string();
-    const location = chance.string();
-    const status = chance.string();
-    const skills = [];
-    const bio = chance.string();
-    const github_username = chance.string();
-    const from = chance.timestamp();
-    const to = chance.integer({ min: from, max: Date.now() });
-    const experience = [];
-    const education = [];
-    const social = {
-      youtube: chance.string(),
-      twitter: chance.string(),
-      facebook: chance.string(),
-      linkedin: chance.string(),
-      instagram: chance.string(),
-    };
+  test("It should create a profile and return status 200", async () => {
+    const userId: string = chance.hash({ length: 24 });
+    const name: string = chance.name();
+    const email: string = chance.email();
+    const avatar: string = chance.url();
+    const profile: IProfile = createProfileMock();
+    const token: string = signJWT({ id: userId, name, email, avatar }, uuid());
+    const insertProfileMock = jest.spyOn(queries, "insertProfile");
+    insertProfileMock.mockImplementation();
 
-    // generate random skills
-    for (let index = 0; index < chance.integer({ min: 1, max: 50 }); index++) {
-      skills.push(chance.string());
-    }
-
-    // generate random experiences
-    for (let index = 0; index < chance.integer({ min: 0, max: 10 }); index++) {
-      experience.push({
-        title: chance.string(),
-        company: chance.string(),
-        location: chance.string(),
-        from,
-        to,
-        current: chance.bool(),
-        description: chance.string(),
-      });
-    }
-
-    for (let index = 0; index < chance.integer({ min: 0, max: 5 }); index++) {
-      education.push({
-        school: chance.string(),
-        degree: chance.string(),
-        field_of_study: chance.string(),
-        from,
-        to,
-        current: chance.bool(),
-        description: chance.string(),
-      });
-    }
-
-    await request(app)
-      .post("/users/register")
-      .set("Content-type", "application/json")
-      .send({ name, email, password });
-    const loginResponse: Response = await request(app)
-      .post("/users/login")
-      .set("Content-type", "application/json")
-      .send({ email, password });
     const response: Response = await request(app)
       .post("/profiles")
       .set("Content-type", "application/json")
-      .set("Authorization", `Bearer ${loginResponse.body.token}`)
+      .set("Authorization", `Bearer ${token}`)
       .send({
-        handle,
-        company,
-        website,
-        location,
-        status,
-        skills,
-        bio,
-        github_username,
-        experience,
-        education,
-        social,
+        ...profile,
       });
 
+    expect(insertProfileMock).toHaveBeenCalled();
     expect(response.status).toBe(201);
+    insertProfileMock.mockRestore();
   });
 
-  test.skip("It should return status 401 if user is not authenticated", async () => {
+  test("It should return status 401 if user is not authenticated", async () => {
     const response: Response = await request(app)
       .post("/profiles")
       .set("Content-type", "application/json")
       .send();
 
     expect(response.status).toBe(401);
+  });
+
+  test("It should return status 400 if handle is undefined", async () => {
+    const userId: string = chance.hash({ length: 24 });
+    const name: string = chance.name();
+    const email: string = chance.email();
+    const avatar: string = chance.url();
+    const profile: IProfile = createProfileMock();
+    const token: string = signJWT({ id: userId, name, email, avatar }, uuid());
+
+    const response: Response = await request(app)
+      .post("/profiles")
+      .set("Content-type", "application/json")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        ...profile,
+        handle: undefined,
+      });
+
+    expect(response.status).toBe(400);
   });
 });
