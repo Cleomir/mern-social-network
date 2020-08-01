6f6ec5fd3ad2d10817af89c696556ff89c201212
Cleomir <cleomir.mg@hotmail.com>
2020-08-01 10:37:32 +0300

    Delete unused code

--- server/src/config/envVariables.ts
+++ server/src/config/envVariables.ts
@@ -1,11 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import dotenv from "dotenv";
 
-if (process.env.NODE_ENV === "test") {
-  dotenv.config({ path: ".env.test" });
-} else {
-  dotenv.config();
-}
+dotenv.config();
 
 export const env: { [key: string]: string } = {
   DB_URL: process.env.MONGO_URL!,
